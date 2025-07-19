c649a3c15d119ca33853bcbaabef5b8b60c482c4
McLo1 <marcelo.conceicao1718@gmail.com>
2025-07-18 21:39:41 -0300

    Criado Query para fazer consulta no banco de dados, das receitas favoritadas

--- src/main/java/com/Cadastro/Receitas/repository/ReceitaRepository.java
+++ src/main/java/com/Cadastro/Receitas/repository/ReceitaRepository.java
@@ -6,12 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ReceitaRepository extends JpaRepository<Receita, Long> {
 
     @Query
     List<Receita> findByTituloContainingIgnoreCaseOrIngredientesContainingIgnoreCase(String titulo, String ingredientes);
+
+    @Query
+    List<Receita> findByFavoritoTrue();
 
 }
