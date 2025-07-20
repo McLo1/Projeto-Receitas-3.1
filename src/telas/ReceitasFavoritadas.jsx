dc97135b90f24957cf810731b5892396cf390d15
McLo1 <marcelo.conceicao1718@gmail.com>
2025-07-20 15:40:20 -0300

    Criado tela separada, onde chamamos o componente Receita Favoritada

---
+++ src/telas/ReceitasFavoritadas.jsx
@@ -0,0 +1,10 @@
+import Favoritos from "../Components/ReceitasFavoritos/Favoritos";
+
+export default function ReceitasFavoritas() {
+
+    return (
+        <div>
+            <Favoritos/>
+        </div>
+    )
+}
