065116e486a643697d98bd13ac3e8206199d21bd
McLo1 <marcelo.conceicao1718@gmail.com>
2025-07-20 15:39:43 -0300

    Criado pasta para salvar o componente Receitas Favoritadas

---
+++ src/Components/ReceitasFavoritos/Favoritos.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import categoriasLegiveis from "../../enums/categoriasLegiveis";
+import './styleFavoritos.css'
+
+export default function Favoritos() {
+
+    const [receitas, setReceitas] = useState([]);
+
+    const navigate = useNavigate();
+
+    const [busca, setBusca] = useState('');
+
+    const handleChange = (event) => {
+        setBusca(event.target.value);
+    }
+
+
+    const BuscarNomIng = () => {
+        axios.get(`http://localhost:8080/favoritos/buscar?termo=${busca}`)
+            .then(response => {
+                setReceitas(response.data);
+                console.log(`Exibindo receitas com a busca ${busca}`);
+            })
+            .catch(error => {
+                console.log("Erro ao buscar receitas", error);
+            })
+    }
+
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/receitas/favoritos')
+            .then(response => {
+                setReceitas(response.data);
+                console.log('Receitas obtidas com sucesso:', response.data);
+            })
+            .catch(error => {
+                console.error('Erro ao obter receitas:', error);
+            });
+    }, []);
+
+
+    const deletarReceita = (id) => {
+        axios.delete(`http://localhost:8080/receitas/${id}`)
+            .then(() => {
+                setReceitas(receitas.filter(receita => receita.id !== id));
+                console.log('Receita deletada com sucesso');
+            })
+            .catch(error => {
+                console.error('Erro ao deletar receita:', error);
+            });
+    }
+
+    const toggleFavorito = (id) => {
+        axios.put(`http://localhost:8080/receitas/${id}/favorito`)
+            .then(res => {
+                console.log('Favorito atualizado', res.data);
+                // Aqui você pode atualizar o estado para refletir no frontend
+            })
+            .catch(err => {
+                console.error('Erro ao atualizar favorito', err);
+            });
+    }
+
+    return (
+        <div>
+
+            <div className="pesquisa-input-button">
+                <input type="text" value={busca} onChange={handleChange} placeholder="Buscar Receitas" />
+                <button onClick={BuscarNomIng}>🔍</button>
+            </div>
+
+            <ul className="lista-receitas">
+                {receitas.map((receita, index) => (
+                    <li key={index}>
+                        <img src={receita.imagemUrl} alt="imagem_da_Receita" width={200} />
+                        <h3>Titulo</h3>
+                        <h2>{receita.titulo}</h2>
+                        <h3>Descrição</h3>
+                        <p>{receita.descricao}</p>
+                        <h3>Ingredientes:</h3>
+                        <p>{receita.ingredientes}</p>
+                        <p>{receita.modoDePreparo}</p>
+                        <p><strong>Categorias:</strong> {categoriasLegiveis[receita.categoria]}</p>
+                        <div className="deletar-favoritar">
+                            <button className="lista-receitas-delete" onClick={() => deletarReceita(receita.id)}>🗑️</button>
+                            <button className="lista-receitas-favoritar" onClick={() => toggleFavorito(receita.id)}>⭐</button>
+                        </div>
+                    </li>
+
+                ))}
+                <button className="lista-receitas-Home" onClick={() => navigate('/')}>Voltar para tela inicial</button>
+
+            </ul>
+        </div>
+    )
+}
